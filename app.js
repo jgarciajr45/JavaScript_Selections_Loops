ba9cc537c69e4a37074526fa45e0457de56c6d3e
Jesus Garcia <jgarciajr45@gmail.com>
2024-07-09 10:14:35 -0700

    exercise 2

--- app.js
+++ app.js
@@ -2,6 +2,27 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
-
+for (let i = 1; i <= 100; i++) {
+    if (i % 2 !== 0) {
+        console.log(i);
+    }
+}
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log('FIZZBUZZ');
+    } else if (i % 3 === 0) {
+        console.log('FIZZ'); 
+    } else if (i % 5 === 0) {
+        console.log('BUZZ');
+    }
+}
+// Exercise 3 Section
+console.log("EXERCISE 3:\n==========\n");
+
+// Exercise 4 Section
+console.log("EXERCISE 4:\n==========\n");
+
+// Exercise 5 Section
+console.log("EXERCISE 5:\n==========\n");
